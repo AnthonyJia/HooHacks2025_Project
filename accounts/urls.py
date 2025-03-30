f624b093b73d64960cf72536baea8d7f37fc6c15
tay <tt262767@gmail.com>
2025-03-30 09:42:52 -0400

    added feed html

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('', views.home, name='home'),
+    path('feed/', views.feed_view, name='feed'),
     path('create/', views.create_post, name='create_post'),
+    path('',views.landing_page, name = 'home'),
+    
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('feed')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
@@ -22,7 +22,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+            return redirect('feed')
     else:
         form = LoginForm()
     return render(request, 'accounts/login.html', {'form': form})
@@ -38,12 +38,16 @@ def create_post(request):
             food_post = form.save(commit=False)
             food_post.owner = request.user  # Set the owner field to the current user
             food_post.save()
-            return redirect('home')  # Redirect to homepage after saving
+            return redirect('feed')  # Redirect to homepage after saving
     else:
         form = FoodPostForm()
     return render(request, 'accounts/create_post.html', {'form': form})
 
-def home(request):
+def feed_view(request):
     # Get all posts that haven't expired
     available_posts = FoodPost.objects.filter(expiration_time__gt=timezone.now())
-    return render(request, 'home.html', {'posts': available_posts})
+
+    return render(request, 'accounts/feed.html', {'posts': available_posts})
+
+def landing_page(request):
+    return render(request,'home.html')
